30ef876c8472d4b2dd6bd794f33d702ef041a53b
Joelle Hili <joelle.hili@outlook.com>
2025-09-17 23:46:34 +0200

    Chore: Set Up Env

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <section>
+      <h1>Forex Exchange</h1>
+      <h2>Checkout the current price on different exchanges for a currency pair</h2>
+    </section>
+    <main>
+      <section>
+        <!-- dropdown - exchange -->
+        <!-- dropdown - symbol -->
+      </section>
+      <!-- graph -->
+    </main>
+  </div>
+</template>
